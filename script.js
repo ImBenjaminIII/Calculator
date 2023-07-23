e7bd28c203d5c9f8cdcdbdccb8c0c46b0dbcf2e6
Benjamin <angelmoreno.am205@gmail.com>
2023-07-23 14:11:27 +0800

    adding functionality to the buttons

--- script.js
+++ script.js
@@ -1,10 +1,9 @@
 let shouldResetScreen = false;
+let currentOperation = null;
 let firstOperand = '';
 let secondOperand = '';
-let currentOperation = null;
 
 const currentOperationScreen = document.querySelector('.current-operation');
-const lastOperationScreen = document.querySelector('.last-operation');
 const allClear = document.querySelector('.clear');
 const number = document.querySelectorAll('.number');
 const deleteNum = document.querySelector('.delete');
@@ -12,10 +11,10 @@ const operatorButton = document.querySelectorAll('.operator');
 
 function clear() {
   currentOperationScreen.textContent = '0';
-  lastOperationScreen.textContent = '';
   firstOperand = '';
   secondOperand = '';
   currentOperation = null;
+  allClear.textContent = 'AC';
 }
 
 function resetScreen() {
@@ -30,18 +29,37 @@ function deleteNumber() {
   );
 }
 
+//Operator Button (-, +, ÷, x, =)
+operatorButton.forEach(buttons => {
+  buttons.addEventListener('click', () => setOperator(buttons.textContent));
+});
+
+// Number Button (., 1, 2, 3, 4, 5, 6, 7, 8, 9)
+number.forEach(number => {
+  number.addEventListener('click', () => {
+    // Reset the screen for the next operand
+    if (currentOperationScreen.textContent == '0' || shouldResetScreen) {
+      resetScreen();
+      allClear.textContent = 'C';
+    }
+    currentOperationScreen.innerHTML += number.textContent;
+  });
+});
+
+// Set operator to the operand
 function setOperator(operator) {
   if (currentOperation !== null) {
     evaluate();
   }
   firstOperand = currentOperationScreen.textContent;
   currentOperation = operator;
-  lastOperationScreen.textContent = `${currentOperationScreen.textContent} ${operator}`;
   shouldResetScreen = true;
 }
 
+// Evaluate the expression
 function evaluate() {
   if (currentOperation === null || shouldResetScreen) return;
+  if (currentOperation === '=') return;
   if (currentOperation === '÷' && currentOperationScreen.textContent === '0') {
     alert('You Cant Divide by zero!');
     return;
@@ -50,10 +68,10 @@ function evaluate() {
   currentOperationScreen.textContent = roundResult(
     operate(currentOperation, firstOperand, secondOperand)
   );
-  lastOperationScreen.textContent = `${firstOperand} ${currentOperation} ${secondOperand} =`;
   currentOperation = null;
 }
 
+// operate the result
 function operate(operator, number1, number2) {
   let num1 = Number(number1);
   let num2 = Number(number2);
@@ -66,7 +84,7 @@ function operate(operator, number1, number2) {
       return num1 + num2;
     case '÷':
       return num1 / num2;
-    case 'x':
+    case '×':
       return num1 * num2;
     case '-':
       return num1 - num2;
@@ -75,22 +93,10 @@ function operate(operator, number1, number2) {
   }
 }
 
+// Round the result to eliminate some numbers in decimal point
 function roundResult(number) {
   return Math.round(number * 1000) / 1000;
 }
-
-operatorButton.forEach(buttons => {
-  buttons.addEventListener('click', () => setOperator(buttons.textContent));
-});
-
-number.forEach(number => {
-  number.addEventListener('click', () => {
-    if (currentOperationScreen.textContent == '0' || shouldResetScreen) {
-      resetScreen();
-    }
-    currentOperationScreen.innerHTML += number.textContent;
-  });
-});
 
 allClear.addEventListener('click', clear);
 deleteNum.addEventListener('click', deleteNumber);
